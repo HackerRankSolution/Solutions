c117620d15b5e007336110d711b50c8781291665
martin.mathew.2k11@gmail.com <martin.mathew.2k11@gmail.com>
2015-11-21 06:54:10 -0800

    Fibonacci Problem

--- src/com/martin/hr/dynamicprogramming/Fibonacci.java
+++ src/com/martin/hr/dynamicprogramming/Fibonacci.java
@@ -1,20 +1,41 @@
 package com.martin.hr.dynamicprogramming;
 
+import java.math.BigInteger;
+import java.util.Scanner;
+
 public class Fibonacci {
 	public static void main(String[] args) {
-		NthTerm(1, 0, 5);
+		
+		Scanner scanner = null;
+		try
+		{
+			scanner = new Scanner(System.in);
+			String line = scanner.nextLine();;
+			String[] tokens = line.split(" ");
+			long nthTerm = Long.parseLong(tokens[0]);
+			long nthPlusTerm = Long.parseLong(tokens[1]);
+			int numOfTerms = Integer.parseInt(tokens[2]);
+			NthTerm(tokens[1],tokens[0] ,numOfTerms);
+		}
+		catch(Exception e)
+		{
+		e.printStackTrace();	
+		}
+		
 	}
 	
 	
-	public static void NthTerm(long nPlus,long n, long numberOfterms)
+	public static void NthTerm(String nPlus,String n, int numberOfterms)
 	{
-		for(int i=2;i<=numberOfterms;i++)
+		BigInteger nPlusBig = new BigInteger(nPlus);
+		BigInteger nBig = new BigInteger(n);
+		for(int i=3;i<=numberOfterms;i++)
 		{
-			long temp = nPlus;
-			nPlus = n+nPlus;
-			n=temp;
+			BigInteger temp = nPlusBig;
+			nPlusBig = nBig.add(nPlusBig.multiply(nPlusBig));
+			nBig=temp;
 		}
-		System.out.println(nPlus);
+		System.out.println(nPlusBig.toString());
 		
 		
 	}
